b611802411b039dd63aef9b0f50e08d2f1cd6002
moinsoft <mmoin.official@gmail.com>
2021-05-24 23:54:45 +0600

    Responsive Test

--- js/main.js
+++ js/main.js
@@ -4,5 +4,15 @@ $(document).ready(function () {
 
   $('.slider__active').bxSlider();
 
+  // meanmenu active
+
+  $('.main__menu').meanmenu({
+
+    meanMenuContainer: '.mobile__menu',
+    meanScreenWidth: "991"
+    
+
+  });
+
 
 });
